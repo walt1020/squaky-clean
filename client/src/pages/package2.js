0f44e5149a753e8fef87782078802baf4c326ee2
Steven McCreery <logad8@gmail.com>
2018-02-11 20:40:50 -0500

    added in dropdown for packages and individual components for each package, overview, pages components

---
+++ client/src/pages/package2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Package2 from "../Components/Packages/package2.js"
+
+
+export class Package2Page extends Component {
+    render() {
+        return (
+            <div>
+                <Package2 />
+            </div>
+            );
+        }
+    }
+export default Package2Page;
---
+++ client/src/pages/package2Details.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Package2Details from "../Components/Packages/package2Details.js"
+
+
+export class Package2DetailsPage extends Component {
+    render() {
+        return (
+            <div>
+                <Package2Details />
+            </div>
+            );
+        }
+    }
+export default Package2DetailsPage;
