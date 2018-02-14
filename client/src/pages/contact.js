a377056b3d67a5cb4dadd7ca123c7d4decd53406
Scott Faber <scottfaberphoto@gmail.com>
2018-02-13 21:07:03 -0500

    styling to scheduling page

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -37,34 +37,34 @@ export class Contact extends React.Component {
 
   render() {
     return (
-      <section>
+      <section className="container">
           <Jumbotron 
             title="Contact Us"
             subTitle="We would love to hear your feedback"
             description="Thank you for being a customer!"
           />
-          <form style={styles.background}className="container">
-            <div className="form-group">
-              <label style={styles.fonts} for="exampleFormControlInput1">Email address</label>
-              <input type="email"
-                    value={this.state.email}
-                      className="form-control"
-                      name="email"
-                      id="exampleFormControlInput1"
-                      onChange={this.handleInputChange}
-                      placeholder="name@example.com" />
-            </div>
-            <div className="form-group">
-              <label style={styles.fonts} for="exampleFormControlTextarea1">Enter your comment here.</label>
-              <textarea className="form-control"
-                        name="body"
-                        value={this.state.body}
-                        id="exampleFormControlTextarea1"
+            <form style={styles.background}>
+              <div className="form-group">
+                <label style={styles.fonts} for="exampleFormControlInput1">Email address</label>
+                <input type="email"
+                      value={this.state.email}
+                        className="form-control"
+                        name="email"
+                        id="exampleFormControlInput1"
                         onChange={this.handleInputChange}
-                        rows="3"></textarea>
-            </div>
-            <button type="submit" onClick={this.handleFormSubmit}>Submit</button>
-          </form>
+                        placeholder="name@example.com" />
+              </div>
+              <div className="form-group">
+                <label style={styles.fonts} for="exampleFormControlTextarea1">Enter your comment here.</label>
+                <textarea className="form-control"
+                          name="body"
+                          value={this.state.body}
+                          id="exampleFormControlTextarea1"
+                          onChange={this.handleInputChange}
+                          rows="3"></textarea>
+              </div>
+              <button type="submit" onClick={this.handleFormSubmit}>Submit</button>
+            </form>
         </section>
     )
   }
--- client/src/pages/scheduleService.js
+++ client/src/pages/scheduleService.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import '../schedule.css';
+// import '../schedule.css';
 import axios from 'axios';
+import '../Components/Packages/package.css';
 
 export class ScheduleService extends React.Component {
   constructor(props){
@@ -34,54 +35,43 @@ export class ScheduleService extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1> Book your appointment with our Mobile Detailing Team!</h1>
-
-        <div className="bootstrap-iso">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                <div className="form-group"> 
-                  <label className="control-label" htmlFor="date">Step 1: Select a Date</label>
-                <input className="date" name="date" type="date"  defaultValue={new Date()} onChange={this.handleChange} />
-                </div>
-              </div>
+      <div className="bootstrap-iso">
+      <h1> Book your appointment with our Mobile Detailing Team!</h1>
+          <div className="container-fluid washPackages row col-xs-8 col-xs-offset-2">
+            <div className="form-group"> 
+              <label className="control-label" htmlFor="date"><br />Step 1: Select a Date: </label>
+              <input className="date" name="date" type="date"  defaultValue={new Date()} onChange={this.handleChange} />
             </div>
-          </div>    
-        </div>
-
-        <div className="bootstrap-iso"> 
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                <label className="control-label" htmlFor="date">Step 2: Select a Time</label>
-               <input type="time"  name="time" onChange={this.handleChange}/>
+              <label className="control-label" htmlFor="date">Step 2: Select a Time: (hh:mm:pm/am)  </label>
+              <input type="time"  name="time" onChange={this.handleChange}/>
             
-              <div className="form-group">
-              <p>*Note that for same day appointment, scheduling will have to be prior to 2pm.</p>
-              <div>     
-          <label className="control-label" htmlFor="address">Step 3: Provide the location of your vehicle</label>
-          <input type="address" name= "address" onChange={this.handleChange} />
-        </div>
-              <br/>
-            <div>  
-            <label className="control-label" htmlFor="car">Step 4: Provide the make, model,color of your vehicle</label>
+            <div className="form-group">
+              <p>*Note that for same day appointment, <br /> scheduling will have to be prior to 2pm.</p>
+            <div>     
+              <label className="control-label" htmlFor="address">Step 3: Provide the location of your vehicle:  </label>
+              <input type="address" name= "address" onChange={this.handleChange} />
+            </div>
+          <br/>
+          
+          <div>  
+            <label className="control-label" htmlFor="car">Step 4: Provide the make, model,color of your vehicle:  </label>
             <input type="car" name="car" onChange={this.handleChange} />
-            </div>
-                      <br/>
-              <div>  
-              <label className="control-label" htmlFor="plate">Step 5: Vehicle license plate number</label>
-              <input type="plate" name="plate" onChange={this.handleChange} />
-              </div>
-              <br/>
-                <button className="btn btn-primary " name="submit" onClick={this.formSubmit}>Submit</button>
+          </div>
+            <br/>
 
-              </div> 
-            </div>
-          </div>        
-        </div>
-    </div>  
-</div>
+          <div>  
+            <label className="control-label" htmlFor="plate">Step 5: Vehicle license plate number:  </label>
+            <input type="plate" name="plate" onChange={this.handleChange} />
+          </div>
+          <br/>
+
+          <button className="btn btn-primary " name="submit" onClick={this.formSubmit}>Submit</button>
+
+        </div> 
+      </div>
+    </div>
+
+
     )
   }
 }
